a9bb6ac7830e7ec136489de5ecdc436f97ba096e
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2024-11-14 13:50:10 -0700

    add stub for recipe info page

---
+++ docs/src/RecipeInfoPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function displayRecipe(recipe_info) {
+    return(
+        <div>
+            <h1>RECIPE</h1>
+        </div>
+    );
+}
+
+export default displayRecipe;
