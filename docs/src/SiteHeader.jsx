5d0dcd6ad0fb92c6fd8d56abdc707c12def6baad
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2024-11-16 00:39:21 -0700

    add tabs to site

--- docs/src/SiteHeader.jsx
+++ docs/src/SiteHeader.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Tabs from './Tabs';
+
+const tabData = [
+    { label: "Restaurants" },
+    { label: "Breakfast" },
+    { label: "Lunch" },
+    { label: "Dinner"},
+    { label: "Dessert"}
+  ];
 
 function displayHeader() {
     return (
         <div>
-            <h1>HEADER</h1>
+             <h1 className="site-name">CopyChef</h1>
+             <div>
+                <Tabs tabs={tabData} />
+             </div>
         </div>
     );
 }
