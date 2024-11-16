d9a3dfe947aa95dffa09a8301afefa04ff14d217
97th Floor <97thfloor@97ths-mbp-6.lan>
2024-11-16 16:46:05 -0700

    recipe page

--- docs/src/RecipeInfoPage.jsx
+++ docs/src/RecipeInfoPage.jsx
@@ -4,6 +4,21 @@ function displayRecipe(recipe_info) {
     return(
         <div>
             <h1>RECIPE</h1>
+            <h2>INGREDIENTS</h2>
+                <ul>
+                    <li>ingredient #1</li>
+                    <li>ingredient #2</li>
+                    <li>ingredient #3</li>
+                    <li>ingredient #4</li>
+                    <li>ingredient #5</li>
+                </ul>
+            <h3>INSTRUCTIONS</h3>
+                <ol>
+                    <li>instruction #1</li>
+                    <li>instruction #2</li>
+                    <li>instruction #3</li>
+                    <li>instruction #4</li>
+                </ol>
         </div>
     );
 }
